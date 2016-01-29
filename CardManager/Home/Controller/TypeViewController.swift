4ab106594563c8c665c4a84ea81fada8364b6ef1
wangyang <wangyang@xunfang.com>
2016-01-29 17:13:44 +0800

    [卡片管家]添加银行卡相关界面

--- CardManager/Home/Controller/TypeViewController.swift
+++ CardManager/Home/Controller/TypeViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class TypeViewController: UITableViewController {
+    
+    let path:String = NSBundle.mainBundle().pathForResource("bank", ofType: "plist")!
+    var bankArray:NSArray = NSArray()
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
         
+        bankArray = NSArray(contentsOfFile: path)!
     }
 
     override func didReceiveMemoryWarning()
@@ -31,14 +34,22 @@ class TypeViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return  20
+        return  bankArray.count
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCellWithIdentifier("type", forIndexPath: indexPath)
-
+        
+        let bankDict:NSDictionary = bankArray[indexPath.row] as! NSDictionary
+        
+        let title:UILabel = cell.viewWithTag(12) as! UILabel
+        let logo:UIImageView = cell.viewWithTag(11) as! UIImageView
+        
+        title.text = bankDict["title"] as? String
+        logo.image = UIImage(named: bankDict["logo"] as! String)
+        
 
         return cell
     }
